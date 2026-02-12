241113933ebfee282913d6a02b5258bad1e46ad5
Pawan Kumar <kumarpawan49995@gmail.com>
2026-02-12 21:43:33 +0530

    Create Segregate 0s and 1s.java

---
+++ Java/Segregate 0s and 1s.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    void segregate0and1(int[] arr) {
+        // code here
+        int countZero=0;
+        int countOne=0;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                countZero+=1;
+            }
+            if(arr[i]==1){
+                countOne+=1;
+            }
+        }
+        int j=0;
+        for(;j<countZero;j++){
+            arr[j]=0;
+        }
+         for(;j<countOne+countZero;j++){
+            arr[j]=1;
+        }
+    }
+}
